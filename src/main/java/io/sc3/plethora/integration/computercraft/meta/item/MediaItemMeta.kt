44ce93e59731051ff6d45a062df79dbe420cca9b
deltanedas <@deltanedas:kde.org>
2025-03-26 20:06:21 +0000

    update MediaItemMeta to use PlatformHelper

--- src/main/java/io/sc3/plethora/integration/computercraft/meta/item/MediaItemMeta.kt
+++ src/main/java/io/sc3/plethora/integration/computercraft/meta/item/MediaItemMeta.kt
@@ -1,12 +1,12 @@
 package io.sc3.plethora.integration.computercraft.meta.item
 
-import dan200.computercraft.impl.MediaProviders
+import dan200.computercraft.shared.platform.PlatformHelper
 import io.sc3.plethora.api.meta.BasicMetaProvider
 import net.minecraft.item.ItemStack
 
 class MediaItemMeta : BasicMetaProvider<ItemStack>() {
   override fun getMeta(target: ItemStack): Map<String, *> {
-    val media = MediaProviders.get(target) ?: return emptyMap<String, Any>()
+    val media = PlatformHelper.get().getMedia(target) ?: return emptyMap<String, Any>()
 
     return mapOf(
       "media" to mapOf(
